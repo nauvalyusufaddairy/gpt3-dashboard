eba2b9b98ada07cd54e33d2d2ed95d166ec9acf2
Nauval Yusuf Addairy <nauvalyusufad@gmail.com>
2022-07-02 11:02:11 +0700

    major changes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
 import React from "react";
-import "./App.css";
+
 import {
+  Footer,
   Blog,
+  Possibility,
   Features,
   WhatGPT3,
   Header,
-  Footer,
-  Possibility,
 } from "./containers";
-import { Brands, Nav, Feature, Article, CTA } from "./components";
-function App() {
-  return (
-    <div className="App">
-      <div className="gradient__bg">
-        <Nav />
-        <Header />
-      </div>
-      <Brands />
-      <WhatGPT3 />
-      <Features />
-      <Possibility />
-      <CTA />
-      <Blog />
-      <Footer />
+import { CTA, Brand, Navbar } from "./components";
+
+import "./App.css";
+
+const App = () => (
+  <div className="App">
+    <div className="gradient__bg">
+      <Navbar />
+      <Header />
     </div>
-  );
-}
+    <Brand />
+    <WhatGPT3 />
+    <Features />
+    <Possibility />
+    <CTA />
+    <Blog />
+    <Footer />
+  </div>
+);
 
 export default App;
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
-export { default as Article } from "./article/Article";
-export { default as Brands } from "./brand/Brands";
-export { default as CTA } from "./cta/CTA";
-export { default as Feature } from "./feature/Feature";
-export { default as Nav } from "./navbar/Nav";
+export { default as Article } from './article/Article';
+export { default as Brand } from './brand/Brand';
+export { default as CTA } from './cta/CTA';
+export { default as Feature } from './feature/Feature';
+export { default as Navbar } from './navbar/Navbar';
